33495b1b1848b9cb8c1bd4147c0bf7a27cc59aaf
unknown <0712024015@163,com>
2019-12-12 11:23:51 +0800

    add files

---
+++ es/zhChars.js
@@ -0,0 +1,12 @@
+import { spell } from './cfg/spell';
+
+const chars2zh = (str) => {
+  // for (let value, key of spell) {
+  //   if (-1 !== spell[key].indexOf(str)) {
+  //     return value;
+  //   }
+  // }
+  return false;
+}
+
+export default chars2zh;
